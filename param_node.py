f2eb78c8d343a8cde32868c7e8c834277e958f72
Blake Hannaford <blake@uw.edu>
2014-10-07 21:14:07 -0700

    changed default rws to os.cwd() for easy startup, fixed comment error

--- param_node.py
+++ param_node.py
@@ -1,22 +1,22 @@
 # ros node generator parameter file
-"""--------------------------------
-(C) 2014, Biorobotics Lab, Department of Electrical Engineering, University of Washington
-This file is part of RNA - The Ros Node Automator.
+#--------------------------------
+#(C) 2014, Biorobotics Lab, Department of Electrical Engineering, University of Washington
+#This file is part of RNA - The Ros Node Automator.
+#
+#    RNA - The Ros Node Automator is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    RNA - The Ros Node Automator is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with RNA - The Ros Node Automator.  If not, see <http://www.gnu.org/licenses/>.
+#--------------------------------
 
-    RNA - The Ros Node Automator is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    RNA - The Ros Node Automator is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with RNA - The Ros Node Automator.  If not, see <http://www.gnu.org/licenses/>.
---------------------------------
-"""
 #  Start by reading and editing this file.   When done, save this and run:
 #  > rng.py
 
@@ -25,9 +25,9 @@ This file is part of RNA - The Ros Node Automator.
 ros_build_system = 'ros_build'
 
 # path to your ROS workspace
-rws = '/home/danying/ROSWorkspace/ros_node_generator/'
-#rws = '/home/blake/Projects/Ros2/RosNodeGen/'
-#rws = '/home/blake/Projects/Ros_node2/RosNodeGen/'
+#examples:
+rws = os.getcwd()   # use the current working directory
+#rws = '/home/user/Projects/ros_workspace/'     # an existing ROS workspace
 # name of ROS package
 pkg = 'my_test_package'
 # package text description
